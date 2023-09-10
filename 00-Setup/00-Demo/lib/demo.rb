70a1ac886d58af2842bfe0992454b26680f9ec68
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-09 18:01:10 -0600

    subo demo :) de nuevo

--- 00-Setup/00-Demo/lib/demo.rb
+++ 00-Setup/00-Demo/lib/demo.rb
@@ -1,5 +1,12 @@
 def circle_area(radius)
-  a = 3.14 * radius*radius
-  return a
+
+  if radius>0
+    a = 3.14 * radius * radius
+    return a
+  else
+    a = 0
+    return a
+  end
+
   # TODO: Implement the `circle_area` method
 end
