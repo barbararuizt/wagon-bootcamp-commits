0171cbbb9122ba11fb4357206f8125c50cf3165e
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-12 21:03:07 -0600

    i did it

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,7 +1,16 @@
 require_relative "compute_name"
 
 # TODO: ask for the first name
+first = gets.chomp
+
 # TODO: ask for the middle name
+middle = gets.chomp
+
 # TODO: ask for the last name
+last = gets.chomp
+
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
+full = compute_name(first, middle, last)
+
+puts "Grettings, #{full} . I wish you a very Merry Christmas!"
