21e4e91881c2cd72f283a7fa9250334648bc9eba
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-14 20:53:58 -0600

    done

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,20 @@
 def coach_answer(your_message)
+  if your_message.downcase == 'i am going to work right now!'
+    ""
+  elsif your_message.include? '?'
+    'Silly question, get dressed and go to work!'
+  else
+    "I don't care, get dressed and go to work!"
+  end
   # TODO: return coach answer to your_message
 end
 
 def coach_answer_enhanced(your_message)
+  if your_message == your_message.upcase && your_message.downcase != "i am going to work right now!"
+    "I can feel your motivation! #{coach_answer(your_message)}"
+
+  else
+    coach_answer(your_message)
+  end
   # TODO: return coach answer to your_message, with additional custom rules of yours!
 end
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,17 @@
 require_relative "coach_answer"
+
+inpt = gets.chomp
+
+until inpt.downcase == 'i am going to work right now!'
+
+  if inpt == inpt.upcase
+    puts coach_answer_enhanced(inpt)
+  else
+    puts coach_answer(inpt)
+  end
+
+  inpt = gets.chomp
+end
+
+
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
