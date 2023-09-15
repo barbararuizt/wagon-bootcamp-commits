a06bbca738a2676111e654c92fe3f4fec0ea096f
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-14 19:18:40 -0600

    Fixed

--- 01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
+++ 01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
@@ -3,7 +3,7 @@
 # - Just look in the doc for the right method of the String, Integer, and Array classes!
 
 def get_rid_of_surrounding_whitespaces(a_string)
-  a_string.strip!
+  a_string.strip
   # TODO: return a copy of the string with leading and trailing whitespaces removed
   # example: get_rid_of_surrounding_whitespaces("  hey yo  ") => "hey yo"
 end
@@ -15,13 +15,13 @@ def belongs_to?(a_string, a_word)
 end
 
 def replace(initial_string, old_letter, new_letter)
-  initial_string.gsub(old_letter,new_letter)
+  initial_string.gsub(old_letter, new_letter)
   # TODO: return a copy of the string with the new letter replacing the old one
   # example: replace("casanova", "a", "o") => "cosonovo"
 end
 
 def exactly_divide(an_integer, a_divider)
-  an_integer/a_divider.to_f
+  an_integer / a_divider.to_f
   # TODO: return division of an_integer by a_divider (computing the floating division, NOT the euclidean one)
   # example: exactly_divide(13, 4) => 3.25
 end
@@ -33,13 +33,13 @@ def divisible_by_two?(an_integer)
 end
 
 def random_subset(an_array, sample_size)
-  (an_array).to_a.sample(sample_size)
+  an_array.sample(sample_size)
   # TODO: return a random subset of an_array with size sample_size
   # example: random_subset(("a".."z").to_a, 4) => ["u", "q", "l", "t"]
 end
 
 def randomize(an_array)
-  an_array.shuffle()
+  an_array.shuffle
   # TODO: return a randomized copy of an_array
   # example: randomize([1, 2, 3, 4]) => [2, 1, 4, 3]
 end
