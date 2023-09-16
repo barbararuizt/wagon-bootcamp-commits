b8ae07526b6183d6a7422a4196a2a5fb5e6b38c1
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-16 14:29:04 -0600

    if in one line and while done

--- 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,20 @@
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  x = 0
+  min > max ? x = -1 : for i in min..max do x = x + i end
+  x
 end
 
 def sum_with_while(min, max)
-  # CONSTRAINT: you should use a while..end structure
+  return -1 if min > max
+
+  x = min
+  value = 0
+
+  while x <= max
+    value += x
+    x += 1
+  end
+
+  value
 end
