12648c1ed65f0a9b982d47f04a13e3a5858e1f28
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-19 10:38:52 -0600

    no offenses

--- 00-Setup/00-Demo/lib/demo.rb
+++ 00-Setup/00-Demo/lib/demo.rb
@@ -1,12 +1,9 @@
 def circle_area(radius)
-
-  if radius>0
+  if radius.positive?
     a = 3.14 * radius * radius
-    return a
   else
     a = 0
-    return a
   end
-
+  a
   # TODO: Implement the `circle_area` method
 end
