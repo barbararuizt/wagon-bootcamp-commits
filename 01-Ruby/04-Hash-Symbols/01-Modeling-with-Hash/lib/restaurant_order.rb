34bdf2042deea1f7ea110c6ec25f07bb6de68f63
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-23 11:28:48 -0600

    first hash

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
@@ -1,3 +1,8 @@
 def poor_calories_counter(burger, side, beverage)
+  menu = {
+    'Hamburger' => 250, 'Cheese Burger'	=> 300, 'Veggie Burger'	=> 540, 'Vegan Burger'	=> 350,
+    'Sweet Potatoes'	=> 230, 'Salad'	=> 15, 'Iced Tea'	=> 70, 'Lemonade'	=> 90
+  }
+  menu[burger] + menu[side] + menu[beverage]
   # TODO: return number of calories for this restaurant order
 end
