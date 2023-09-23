c4b84862c8cbd7a6b1bdf101789bcfe391b4e581
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-23 12:27:22 -0600

    other option for combos added

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
@@ -9,10 +9,26 @@ DISHES_CALORIES = {
   "Lemonade" => 90
 }
 
+COMBOS = {
+  "Cheesy Combo" => ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
+  "Veggie Combo" => ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
+  "Vegan Combo" => ["Vegan Burger", "Salad", "Lemonade"]
+}
+
 def poor_calories_counter(burger, side, beverage)
   DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
 end
 
 def calories_counter(orders)
+  x = 0
+  orders.each do |meal|
+    if COMBOS.key?(meal)
+      x += poor_calories_counter(COMBOS[meal][0], COMBOS[meal][1], COMBOS[meal][2]) # siempre 3
+      # COMBOS[meal].each { |dish| x += DISHES_CALORIES[dish] } - más flexible pero más lento
+    else
+      x += DISHES_CALORIES[meal].to_i
+    end
+  end
+  x
   # TODO: return number of calories for a less constrained order
 end
