9f05d4b181ccf59a9b0654d90a536badabddb5d4
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-23 12:49:58 -0600

    ok

--- 01-Ruby/04-Hash-Symbols/03-On-symbols/lib/symbols.rb
+++ 01-Ruby/04-Hash-Symbols/03-On-symbols/lib/symbols.rb
@@ -8,10 +8,12 @@
 # irb> "foo".object_id
 
 def are_identical_symbols_same_objects?
+  true
   # TODO: Answer the question by making this method to return true or false
 end
 
 def are_identical_strings_same_objects?
+  false
   # TODO: Answer the question by making this method to return true or false
 end
 
@@ -21,17 +23,25 @@ end
 # - https://ruby-doc.org/core-2.7.5/Symbol.html
 
 def convert_string_to_symbol(a_string)
+  a_string.to_sym
   # TODO: return the symbol version of the parameter `a_string` passed to this method
 end
 
 def convert_symbol_to_string(a_symbol)
+  a_symbol.to_s
   # TODO: return the string version of the parameter `a_symbol` passed to this method
 end
 
 def me
+  {
+    age: 21,
+    name: "barbara",
+    hobby: "my cat"
+  }
   # TODO: return a Hash representing yourself, with keys such as age and name
 end
 
 def fruits
+  ["apple", "pear", "watermelon"]
   # TODO: return an array of fruits
 end
