18be0b542749a019f00bab1b0253272685db32e2
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-22 19:35:55 -0600

    took a while and not the most optimal

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,3 +1,33 @@
+def duplicates(array)
+  dupes = []
+  array.each do |e|
+    dupes << e if array.count(e) > 1
+  end
+
+  dupes.empty?
+end
+
 def colorful?(number)
-  # TODO: return true if the number is colorful, false otherwise
+  arr = number.to_s.chars
+
+  if number.class != Integer
+    false
+
+  elsif number < 10
+    true
+
+  elsif arr.size == 2
+    x = arr[0].to_i
+    y = arr[1].to_i
+    comp = [x, y, x * y]
+    duplicates(comp)
+
+  elsif arr.size == 3
+    x = arr[0].to_i
+    y = arr[1].to_i
+    z = arr[2].to_i
+    comp = [x, y, z, x * y, y * z, x * y * z]
+    duplicates(comp)
+
+  end
 end
