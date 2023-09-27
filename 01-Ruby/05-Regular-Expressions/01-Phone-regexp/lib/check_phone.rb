76e00ff2eb17362db9389d3427256688d6f7a0f8
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-26 21:40:07 -0600

    ok

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -2,7 +2,6 @@ def french_phone_number?(phone_number)
   # TODO: true or false?
   zero = /^(\+33)?[^2][^0]((\s|-)?\d{2}){4}$/
   phone_number.match?(zero)
-  # 0[^0](\D?(\d*))
   # ((\s-)?\d{2})*
   # (\+33)?[^2][^0]\s buscar 33 y contempla inicio sin 2 y sin 0
 end
