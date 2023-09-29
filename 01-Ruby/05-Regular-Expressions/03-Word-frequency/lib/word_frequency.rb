94deb788bd4d41519a91f983a61c3611d47e431c
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-28 20:09:48 -0600

    finally done, one regex only

--- 01-Ruby/05-Regular-Expressions/03-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/03-Word-frequency/lib/word_frequency.rb
@@ -1,3 +1,14 @@
 def most_common_words(file_name, stop_words_file_name, number_of_word)
-  # TODO: return hash of occurences of number_of_word most frequent words
+  count = Hash.new(0)
+  arr = []
+
+  File.open(stop_words_file_name, "r").each_line { |line| arr << line.chomp.to_s }
+
+  File.open(file_name, "r").each_line do |line|
+    words = line.chomp.downcase.split(/\W+/)
+    words.each { |word| count[word] += 1 unless arr.include?(word) }
+  end
+
+  count = count.sort_by { |_key, value| -value }
+  count.first(number_of_word).to_h
 end
