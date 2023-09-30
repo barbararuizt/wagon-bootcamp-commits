f05c54109a458eb5dc321500fe850ac72cb15e6d
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-30 12:56:37 -0600

    most succesfull movies before n year sorted by descending earnings

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,5 +1,13 @@
 require 'csv'
 
 def most_successful(number, max_year, file_path)
+  mov = []
+
+  CSV.foreach(file_path) do |row|
+    year = row[1].to_i
+    mov << { name: row[0], year: year, earnings: row[2].to_i } if year < max_year
+  end
+
+  mov.sort_by { |hsh| -hsh[:earnings] }.first(number)
   # TODO: return an array of most successful movies before `max_year`
 end
