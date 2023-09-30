5fb52cdb4c5720309b73e622179b204d312797ca
Barbara Ruiz <barbararuiz456@outlook.com>
2023-09-30 16:20:44 -0600

    Word game

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -2,9 +2,27 @@ require 'open-uri'
 require 'json'
 
 def generate_grid(grid_size)
+  grid = []
+  (1..grid_size).each { |_n| grid << ('A'..'Z').to_a.sample }
+  grid
   # TODO: generate random grid of letters
 end
 
+def grid?(grid, word)
+  word.chars.all? { |letter| grid.count(letter.upcase) >= word.count(letter) }
+end
+
 def run_game(attempt, grid, start_time, end_time)
-  # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
+  url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+  serialized = URI.open(url).read
+  check = JSON.parse(serialized) # se hace Hash {"found"=>true, "word"=>"apple", "length"=>5}
+
+  calc_time = end_time - start_time
+  results = { time: calc_time, score: 0, message: "not an english word or not in the grid" }
+
+  if check["found"] == true && grid?(grid, attempt) == true
+    results[:score] = 80 - calc_time + attempt.size
+    results[:message] = "Yeah! Well done"
+  end
+  results
 end
