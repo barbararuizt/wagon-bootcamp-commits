cf3dfaf6bba5bd1c726f72d46731d09f6e68b06c
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-05 21:07:14 -0600

    unfinished comparable

--- 02-OOP/01-OO-Basics/Optional-01-Richest-Student/lib/student.rb
+++ 02-OOP/01-OO-Basics/Optional-01-Richest-Student/lib/student.rb
@@ -1,2 +1,19 @@
 class Student
+    attr_accessor :name
+    include Comparable
+
+    def <=>(other)
+        @fives <=> other.fives
+    end
+
+    def initialize(name, fives, tens, twenties)
+        @name = name
+        @fives = 5 * fives
+        @tens = 10 * tens
+        @twenties = 20 * twenties
+    end
+
+    def wealth
+        @fives + @tens + @twenties
+    end
 end
