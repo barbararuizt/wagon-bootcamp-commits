a8e68f6072d69337ada7eaab89ed9c16f292ff36
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-07 15:33:17 -0600

    acumulable para promedio

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,24 @@
 class Restaurant
+  attr_reader :city, :name, :average_rating
+
   # TODO: add relevant accessors if necessary
 
   def initialize(city, name)
+    @city = city
+    @name = name
+    @total_ratings = 0
+    @sum_ratings = 0
     # TODO: implement constructor with relevant instance variables
   end
 
   # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    @total_ratings += 1 # acumula cosas al ser el mismo objeto
+    @sum_ratings += new_rate
+    @average_rating = @sum_ratings / @total_ratings
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select { |restaurant| restaurant.city == city }
+  end
 end
