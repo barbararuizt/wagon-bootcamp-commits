e8f83e6198b666b56aa8ffc1a741790a42027675
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-07 16:32:30 -0600

    parent classes mastering

---
+++ 02-OOP/02-OO-Advanced/03-Farming-Diary-Animals/lib/chicken.rb
@@ -0,0 +1,25 @@
+require_relative "animal"
+
+class Chicken < Animal
+  attr_reader :eggs
+
+  def initialize(gender)
+    super()
+    @gender = gender
+    @eggs = 0
+  end
+
+  def talk
+    case @gender
+    when 'female'
+      'cluck cluck'
+    when 'male'
+      'cock-a-doodle-doo'
+    end
+  end
+
+  def feed!
+    super
+    @eggs += 2 if @gender == 'female'
+  end
+end
