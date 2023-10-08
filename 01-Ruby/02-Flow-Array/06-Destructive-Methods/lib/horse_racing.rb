52ff3dac63997090990ba1dcf9d1ba6a31cc4c02
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-08 14:41:06 -0600

    destructive reverse & map

--- 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
@@ -1,3 +1,7 @@
 def horse_racing_format!(race_array)
+  race_array.reverse!.map! do |horse|
+    "#{race_array.size - race_array.index(horse)}-#{horse}!"
+  end
+  race_array
   # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
 end
