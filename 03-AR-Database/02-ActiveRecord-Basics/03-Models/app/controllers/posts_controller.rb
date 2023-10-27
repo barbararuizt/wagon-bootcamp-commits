c99cbe8871b12c4f71e3478b02497bd511d8ea7d
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-26 19:38:24 -0600

    model and controller using active records for DB

--- 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
@@ -6,27 +6,43 @@ class PostsController
   end
 
   def index
+    @view.all(Post.all)
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
   end
 
   def create
+    title = @view.ask("title")
+    url = @view.ask("url")
+    Post.new(title: title, url: url, votes: 0).save
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
   end
 
   def update
+    index
+    id = @view.ask("id")
+    title = @view.ask("new title")
+    url = @view.ask("new url")
+    Post.find(id).update(title: title, url: url)
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
   end
 
   def destroy
+    index
+    id = @view.ask("id")
+    Post.find(id).destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
   end
 
   def upvote
+    index
+    id = @view.ask("id")
+    post = Post.find(id)
+    post.update(votes: post.votes + 1)
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
   end
--- 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,13 @@
 class PostsView
+  def all(posts)
+    posts.each do |post|
+      puts "#{post.id} - #{post.title} - #{post.url} - #{post.votes}"
+    end
+  end
+
+  def ask(thing)
+    puts "What is the #{thing}?"
+    gets.chomp
+  end
   # TODO: implement some methods here when controller needs to `puts` or `gets`
 end
