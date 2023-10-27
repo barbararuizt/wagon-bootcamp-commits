bcc9077f39efeb71e6effd9c7956bc75cc1eb706
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-26 20:33:37 -0600

    use of API

--- 03-AR-Database/02-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -1,4 +1,13 @@
 require "json"
 require "rest-client"
 
+response = RestClient.get 'https://hacker-news.firebaseio.com/v0/topstories.json'
+top_stories = JSON.parse(response.body)
+top_10_stories = top_stories[0..9]
+
+top_10_stories.each do |id|
+  response = RestClient.get "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+  story = JSON.parse(response.body)
+  Post.create(title: story["title"], url: story["url"], votes: story["score"])
+end
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
