0b65b5524d2b5b0807e9d0cdfa459bfaa5682746
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-26 20:23:22 -0600

    introduction of faker into DB Active Records

--- 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,6 @@
 require "faker"
+
+100.times do
+  Post.create(title: Faker::Quotes::Shakespeare.as_you_like_it_quote, url: Faker::Internet.url, votes: rand(1..1000))
+end
 # TODO: Write a seed to insert 100 posts in the database
