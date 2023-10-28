1d896c87bab27e2cfe56b2d95db9ef8aaf6f8951
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-28 12:51:31 -0600

    First push

--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
+  validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: false }
+  validates :url, presence: true, format: { with: /\Ahttps?:\/\/[\S]+\z/ }
+  validates :user, presence: true
   # TODO: Add some validation
 end
--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
 
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   # TODO: Add some validation
   # TODO: Add some callbacks
 end
