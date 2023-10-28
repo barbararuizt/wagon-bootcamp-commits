5a3b4798e54a5932cd8f9d3e2c0341f094dc2ae7
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-28 11:43:21 -0600

    Creation, migration, seeding and 1:N relationship stating

--- 03-AR-Database/03-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,14 @@
 # TODO: Write a seed
+require 'faker'
+
+5.times do
+  name = Faker::Artist.name
+  User.create(username: name, email: "#{name.downcase.gsub(' ', '_')}@live.com")
+end
+
+User.all.each do |user|
+  num = rand(5..10)
+  num.times do
+    Post.create(title: Faker::Verb.ing_form, url: Faker::Internet.url, user_id: user.id)
+  end
+end
