90379b6c1c4e84f0d722ee499ba031311670a73e
Barbara Ruiz <barbararuiz456@outlook.com>
2023-10-28 13:10:05 -0600

    Added before validation and after creation methods

--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
   has_many :posts
-
+  before_validation :clean_username
+  after_create :welcome_email
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
   # TODO: Add some validation
+
+  private
+
+  def clean_username
+    self.email = email.strip unless email.nil?
+  end
+
+  def welcome_email
+    FakeMailer.instance.mail(email, "Welcome to HN!")
+  end
   # TODO: Add some callbacks
 end
