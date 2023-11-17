0488d30432c0fcfe749088fc58044d86d20c71c4
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-16 20:49:16 -0600

    item select and modify totals - lottery

--- 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
+++ 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
@@ -1,1 +1,17 @@
 // TODO: Write your gambling application here.
+const cards = document.querySelectorAll(".scratchcard");
+let money = document.getElementById("balance").innerText;
+money = parseInt(money, 10);
+
+cards.forEach((card) => {
+  card.addEventListener("click", (event) => {
+    if (money > 0) {
+      const won = card.dataset.amount;
+      card.innerText = won;
+      money = money - 10 + parseInt(won, 10);
+      card.dataset.scratched = "true";
+      event.currentTarget.classList.add("disabled");
+      document.getElementById("balance").innerText = money;
+    }
+  });
+});
