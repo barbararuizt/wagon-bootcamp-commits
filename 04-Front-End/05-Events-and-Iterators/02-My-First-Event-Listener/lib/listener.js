32c03482551673afbb9bf6964a492d2619da15b0
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-16 19:23:34 -0600

    Event listener for button

--- 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,7 @@
 // TODO: React to a click on the button!
+const button = document.getElementById("clickme");
+button.addEventListener("click", (event) => {
+  event.currentTarget.classList.add("disabled");
+  event.currentTarget.innerText = "Bingo!";
+  event.currentTarget.play("sound.mp3");
+});
