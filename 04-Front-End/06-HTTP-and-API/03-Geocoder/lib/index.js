57e59e55474710d66d2618afc9dc9e2b89b96d62
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-18 15:32:02 -0600

    fetch input and get API map

--- 04-Front-End/06-HTTP-and-API/03-Geocoder/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/03-Geocoder/lib/index.js
@@ -1,12 +1,27 @@
-// TODO: Create a function to get the coordinates from an address and display a map with a marker on it
 const showMap = (userInput) => {
   // TODO: Construct the URL (with apiKey & userInput) and make the fetch request to the mapbox API
-  const url = `...`;
+  const token = "pk.eyJ1IjoiYmFyYmFyYXJ1aXp0IiwiYSI6ImNscDRlaGN0bzE3dWQyanF1M2NnaG5tNHYifQ.4xFGzzsci63Osa_u7YNlcw";
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${userInput}.json?access_token=${token}`;
 
   fetch(url)
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((data) => {
-      console.log(data);
+      const coord = data.features[0].center;
+      const area = document.querySelector("p");
+      console.dir(area);
+      area.innerText = coord;
+
+      mapboxgl.accessToken = 'pk.eyJ1IjoiYmFyYmFyYXJ1aXp0IiwiYSI6ImNscDRlaGN0bzE3dWQyanF1M2NnaG5tNHYifQ.4xFGzzsci63Osa_u7YNlcw';
+      const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: coord, // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+
+      new mapboxgl.Marker()
+        .setLngLat(coord)
+        .addTo(map);
       // TODO: Insert the info into the DOM
       // - Extract the coordinates from the parsed JSON response (lang, lat)
       // - Display the coordinates in the element where the coordinates will be displayed
@@ -14,9 +29,17 @@ const showMap = (userInput) => {
       // - Add a marker to the map at the coordinates
     });
 };
-//
+
 // TODO: Select the form element
 // TODO: Add event listener to the form that:
-// - Prevents the default form submission behavior
-// - Get the user input
+
+function handleSubmit(event) {
+  event.preventDefault(); // - Prevents the default form submission behavior
+  const value = document.querySelector("#input").value; // - Get the user input // name attribute in html
+  showMap(value);
+}
+
+const form = document.querySelector("form");
+const ubicacion = form.addEventListener('submit', handleSubmit);
+
 // - Calls the showMap function with the user input as an argument
