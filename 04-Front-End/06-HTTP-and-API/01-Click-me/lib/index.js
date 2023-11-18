76e75ef7e96ba54ac68854088087d73893ef0173
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-18 12:03:17 -0600

    button click alert

--- 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
@@ -1,5 +1,9 @@
 const displayAlertOnButtonClick = () => {
+  const button = document.querySelector("button");
   // TODO: Select the big green button
+  button.addEventListener("click", () => {
+    alert("Thank you!");
+  });
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
 };
