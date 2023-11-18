ba24e3df84c41b530bd5a1ca24fc4dfc5d8189c6
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-18 12:19:43 -0600

    click and highlight with no more than 1 indentation

--- 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,18 @@
 // TODO: write your code here!
+const boxes = document.querySelectorAll(".clickable");
+
+// boxes.forEach(box => {
+//   box.addEventListener("click", (event) => {
+//     event.currentTarget.classList.toggle("active")
+//   })
+// })
+
+const highlight = (event) => {
+  event.currentTarget.classList.toggle("active");
+};
+
+const chooseBox = (box) => {
+  box.addEventListener('click', highlight);
+};
+
+boxes.forEach(chooseBox);
