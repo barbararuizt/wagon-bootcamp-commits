096e3a3a111013fd0f87870c94bb433cf118bf23
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-21 20:04:30 -0600

    fetch api and insert into html

--- 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
+++ 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
@@ -1,11 +1,18 @@
-const agifyApiUrl = "https://api.agify.io/"
+const agifyApiUrl = "https://api.agify.io/";
 
 const displayAge = (event) => {
   event.preventDefault();
   const firstName = document.getElementById("first-name").value;
+  // https://api.agify.io?name=THE_FIRST_NAME
+  const url = `https://api.agify.io?name=${firstName}`;
+  fetch(url)
+    .then(response => response.json()) // Wait for the response and parse it as JSON
+    .then((data) => {
+      const ageContainer = document.getElementById("your-age");
+      ageContainer.innerText = `Your estimated age is: ${data.age}!`;
+    });
   // TODO: Display your age with an AJAX call instead of the console.log()
-  console.log(firstName);
-}
+};
 
 const form = document.getElementById("fetch-age");
 form.addEventListener("submit", displayAge);
