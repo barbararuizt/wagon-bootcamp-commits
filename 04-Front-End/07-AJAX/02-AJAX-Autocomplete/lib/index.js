9368898df3a21ac6efbc93bded3d049e73f5069e
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-21 20:57:31 -0600

    fetch api and update on each keyboard click

--- 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,22 @@
 // TODO: Autocomplete the input with AJAX calls.
+
+const displayResults = (event) => {
+  event.preventDefault();
+  const input = document.getElementById("search").value;
+  const url = `https://wagon-dictionary.herokuapp.com/autocomplete/${input}`;
+  fetch(url)
+    .then(response => response.json()) // Wait for the response and parse it as JSON
+    .then((data) => {
+      let list = "";
+      data.words.forEach((element) => {
+        list += `<li>${element}</li>`;
+      });
+
+      const results = document.getElementById("results");
+      results.innerHTML = list;
+    });
+};
+
+
+const container = document.getElementById("container");
+container.addEventListener("keyup", displayResults);
