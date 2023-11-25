1d0ac9c2b13e3c70216b2b8445a792b98327dabd
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-25 11:58:30 -0600

    creating html list with array of hashes

--- 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
+++ 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
@@ -6,9 +6,21 @@ const todos = [
 ];
 
 // To-do item HTML 👇
-// <div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
-//   <input class="d-flex form-check-input me-1" type="checkbox">
-//   <div>Replace with to-do title</div>
-// </div>
+const list = document.getElementById("todosContainer");
+let content = "";
+
+todos.forEach((task) => {
+  let check = "";
+  if (task.done) {
+    check = "checked";
+  }
+  const elem = `<div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
+    <input class="d-flex form-check-input me-1" type="checkbox" ${check}>
+    <div>${task.title}</div>
+  </div>`;
+  content += elem;
+});
+
+list.innerHTML = content;
 
 // TODO: Dynamically generate a list of to-dos based on `todos` array, and display them
