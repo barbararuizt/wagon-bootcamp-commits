7de5ff2caa9db8df975c8d05351b4cd7d1f99a2f
Barbara Ruiz <barbararuiz456@outlook.com>
2023-11-25 12:11:57 -0600

    creating html list with array of hashes using

--- 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
+++ 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
@@ -5,4 +5,15 @@ const todos = [
   { title: "Water the plants", done: true }
 ];
 
+const template = document.getElementById("todoItemTemplate");
+const body = document.querySelector("body");
+
+todos.forEach((task) => {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector('input').checked = task.done;
+  clone.querySelector('.title').textContent = task.title;
+  body.appendChild(clone);
+});
+
+
 // TODO: Use the template in the `index.html` to dynamically generate a list based on `todos` array
